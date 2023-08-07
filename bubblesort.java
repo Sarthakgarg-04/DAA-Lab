8019b37d3d5f769e38e1b3d1a5745f71339e9c57
Sarthakgarg-04 <98890517+Sarthakgarg-04@users.noreply.github.com>
2023-08-07 20:20:14 +0530

    My new 2 files created

---
+++ bubblesort.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class bubblesort
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string value");
+        String sample = input.nextLine();
+
+        System.out.println("string entered is "+sample);
+    }
+}
+
+
